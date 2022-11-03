d8ce0d41d3e2f97111713e555c8026365dbd163b
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-11-03 17:40:36 +0200

    add email validation

--- src/componant/Contact.js
+++ src/componant/Contact.js
@@ -1,9 +1,24 @@
-import React from "react"; 
+import React,{useEffect,useRef,useState} from "react"; 
 import emailjs from '@emailjs/browser'
 
+const EMAIL_REGX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
 export default function Contact(){
+   
+    const nameRef = useRef();
+    const [email,setEmail]=useState("")
+    const [validEmail, setValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
     const form = React.useRef();
- 
+    useEffect(() => {
+        nameRef.current.focus();
+      }, []);
+      useEffect(() => {
+        setValidEmail(EMAIL_REGX.test(email));
+      }, [email]);
+
+
+      //email js 
     const sendEmail = (e) => {
       e.preventDefault(); // prevents the page from reloading when you hit “Send”
     
@@ -22,11 +37,20 @@ export default function Contact(){
         <section className="contact">
             <form ref={form} onSubmit={sendEmail}>
             <label>name</label>
-            <input placeholder="name" name="user_name"></input>
-            <label>email</label>
-            <input placeholder="email" name="user_email"></input>
+            <input autoComplete="off" ref={nameRef} placeholder="name" name="user_name"></input>
+            <label>email  <label style={{color:"red"}} color="red" className={emailFocus && email && !validEmail ? "visible" : "hidden"}>
+            <br/> ONLY valid email
+            </label> </label>
+            <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="off"
+            required
+            onFocus={() => setEmailFocus(true)}
+            onBlur={() => setEmailFocus(false)}
+             placeholder="email" name="user_email"></input>
             <label>message</label>
-            <textarea placeholder="I am waiting  to hear from YOU : " name="message" />
+            <textarea autoComplete="off" placeholder="I am waiting  to hear from YOU : " name="message" />
             <button type="submit">submit</button>
             </form> 
         </section>
