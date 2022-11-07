e4d3d257ccfd17c870a06dd8cb5e1721bfe37e53
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-11-07 21:16:19 +0200

    add to my projects

--- src/component/Project/myProjects.js
+++ src/component/Project/myProjects.js
@@ -1,32 +1,32 @@
 const myProjects = {
-    p1: {
-      image: require("../../images/h4isite.png"),
-      title: "HI COMMERCE",
-      description:
-        "Fully featured Ecommerce website with currency switcher, cart, category, and PDP andJWT authentication",
-      technologies: ["REACT", "apollo", "graphQL", "redux"],
-    },
-    p2: {
-      image: require("../../images/tmlawysite.png"),
-      title: "EDU Management ",
-      description:
-        ". Help managing student state and many courses info managing payment .",
-      technologies: ["MUI", "REACT", "axios"],
-    },
-    p3: {
-      image: require("../../images/VBCc.png"),
-      title: "Vaccine verification system powered by Blockchain ",
-      description:
-        "This system implemented and designed to provide the authenticity and validity of the data using blockchain technology and keeping in mind being user-friendly and easy to use on any device that uses a web browser and almost impossible to hack.",
-      technologies: ["react-router", "REACT", "axios"],
-    },
-    p4: {
-      image: require("../../images/tmlawysite.png"),
-      title: "EDU Management ",
-      description:
-        ". Help managing student state and many courses info managing payment .",
-      technologies: ["MUI", "REACT", "axios"],
-    },
-  };
+  p1: {
+    image: require("../../images/h4isite.png"),
+    title: "HI COMMERCE",
+    description:
+      "Fully featured Ecommerce website with currency switcher, cart, category, and PDP andJWT authentication",
+    technologies: ["REACT", "apollo", "graphQL", "redux"],
+  },
+  p2: {
+    image: require("../../images/VBCc.png"),
+    title: "Vaccine verification system powered by Blockchain ",
+    description:
+      "This system implemented and designed to provide the authenticity and validity of the data using blockchain technology and keeping in mind being user-friendly and easy to use on any device that uses a web browser and almost impossible to hack.",
+    technologies: ["react-router", "REACT", "axios"],
+  },
+  p3: {
+    image: require("../../images/tmlawysite.png"),
+    title: "EDU Management ",
+    description:
+      ". Help managing student state and many courses info managing payment .",
+    technologies: ["MUI", "REACT", "axios"],
+  },
+  p4: {
+    image: require("../../images/Wordle.png"),
+    title: "Wordle Game ",
+    description:
+      "A Clone of the famous Wordle game with word categories.",
+    technologies: ["Vanilla JS", "html", "CSS"],
+  },
+};
 
-  export default myProjects;
+export default myProjects;
