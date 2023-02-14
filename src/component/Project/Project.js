a2afa072cb35e46330ab6aee149c5c9f93d88698
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-02-14 21:43:21 +0200

    #

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -3,30 +3,25 @@ import ProjectCard from "./ProjectCard";
 import myProjects from "./myProjects";
 import { Link } from "react-router-dom";
 
-
-function displayedProject(displayedNumber){
-  var Cards=[]
-  for( let i = 1; i<=displayedNumber ; i++ ){
-    Cards.push( <ProjectCard myProject={myProjects[`p${i}`]} />)
+function displayedProject(displayedNumber) {
+  var Cards = [];
+  for (let i = 1; i <= displayedNumber; i++) {
+    Cards.push(<ProjectCard myProject={myProjects[`p${i}`]} />);
   }
-  return Cards
+  return Cards;
 }
 
 export default function Project(props) {
   return (
     <section className="project">
       <h3>PROJECTS</h3>
-      <div className="grid">
-        {
-          displayedProject(props.displayedNumber)
-        }
-      </div>
-      
-      
-         { props.displayedNumber<3 && <Link to="potfolio/Projects">
-          
-            <button>view more Projects</button>
-          </Link>}
+      <div className="grid">{displayedProject(props.displayedNumber)}</div>
+
+      {props.displayedNumber < 3 && (
+        <Link to="potfolio/Projects">
+          <button>view more Projects</button>
+        </Link>
+      )}
     </section>
   );
 }
